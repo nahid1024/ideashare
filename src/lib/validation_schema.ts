9c985bad19fc2aa9470798cb9437f564b84cd262
nahid1024 <nahid1024bit@gmail.com>
2026-06-03 21:05:57 +0600

    Create validation_schema.ts

---
+++ src/lib/validation_schema.ts
@@ -0,0 +1,14 @@
+import { z } from 'zod';
+
+export const signupSchema = z
+	.object({
+		first_name: z.string().min(2, 'Name must be at least 2 characters'),
+		last_name: z.string().min(2, 'Name must be at least 2 characters'),
+		email: z.email('Invalid email address'),
+		password: z.string().min(8),
+		confirm_password: z.string()
+	})
+	.refine((data) => data.password === data.confirm_password, {
+		message: "Passwords don't match",
+		path: ['confirm_password']
+	});
