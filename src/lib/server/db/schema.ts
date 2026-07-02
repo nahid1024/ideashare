0b347f8d0311dbb0c7492e57cdae5a00df1e66ad
nahid1024 <nahid1024bit@gmail.com>
2026-07-03 00:44:18 +0600

    Created full database schema

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,9 +1,8 @@
-import { pgTable, serial, integer, text } from 'drizzle-orm/pg-core';
-
-export const task = pgTable('task', {
-	id: serial('id').primaryKey(),
-	title: text('title').notNull(),
-	priority: integer('priority').notNull().default(1)
-});
-
-export *  from './auth.schema';
+export * from './auth.schema';
+export * from './posts';
+export * from './media';
+export * from './postTags';
+export * from './refinements';
+export * from './share';
+export * from './sparked';
+export * from './tags';
